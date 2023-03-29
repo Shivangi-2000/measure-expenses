146870502b56cd8aec62f4d62bd9a0924888a1ff
Shivangi Gupta <71278498+Shivangi-2000@users.noreply.github.com>
2023-03-29 14:22:33 +0530

    filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,26 +13,19 @@ export default function Expenses(props) {
     <>
       <Card className="expenses">
         <ExpensesFilter selected={selectedYear} getYear={yearFilterHandler} />
-        <ExpenseItem
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.expenses[1].title}
-          amount={props.expenses[1].amount}
-          date={props.expenses[1].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.expenses[2].title}
-          amount={props.expenses[2].amount}
-          date={props.expenses[2].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.expenses[3].title}
-          amount={props.expenses[3].amount}
-          date={props.expenses[3].date}
-        ></ExpenseItem>
+        {props.expenses
+          .filter(function (flt) {
+            console.log(`flt`, flt, flt.date.getFullYear(), selectedYear);
+            return flt.date.getFullYear().toString() === selectedYear;
+          })
+          .map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            ></ExpenseItem>
+          ))}
       </Card>
     </>
   );
